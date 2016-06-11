3d3817f8cf47e4b958807430f7e3fe1d8597f494
wladek.airo@gmail.com <wladek.airo@gmail.com>
2016-06-12 00:37:36 +0300

    reward pojo

---
+++ src/main/java/com/wladek/pakmart/pojo/RewardForm.java
@@ -0,0 +1,31 @@
+package com.wladek.pakmart.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by wladek on 6/12/16.
+ */
+
+/**
+ * Hold the values for points reward and redeeming simulation
+ */
+public class RewardForm {
+    private String phoneNumber;
+    private BigDecimal amount;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
